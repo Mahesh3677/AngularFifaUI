a4e697c48d78130fceadf7f3af115489c9590f80
Mahesh K Chacko <mahesh.chacko@speridian.com>
2021-09-05 15:25:41 +0530

    Dashboard added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegistrationComponent } from './user/registration/registration.component';
+
 import { LoginComponent } from './user/login/login.component';
-
-
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [RegistrationComponent, LoginComponent],
+  declarations:
+   [
+     LoginComponent,
+     DashboardComponent,
+     FooterComponent,
+     HeaderComponent,
+     NavbarComponent,
+     HomeComponent
+     ],
   imports: [
-    CommonModule
-  ]
+    CommonModule ,
+    AdminRoutingModule
+  ],
+  exports: [HomeComponent ,
+    HeaderComponent,
+  NavbarComponent,
+FooterComponent,
+DashboardComponent]
 })
 export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AdminModule} from './admin/admin.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
